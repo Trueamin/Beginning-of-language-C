cacd698764b5fc9bf1b274edfcf951a7457fb6ac
Mohammad Amin <150001455+Trueamin@users.noreply.github.com>
2023-11-15 10:34:58 +0330

    Add files via upload

---
+++ Project6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    //arithemic operators
+
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ increment
+    // -- decrement
+
+    int x = 5;
+    int y = 2;
+
+    //int z = x + y;
+    //int z = x - y;
+    //int z = x * y;
+    //int z = x / (float) y;
+
+    
+    x++;
+    y--;
+
+    printf("%d", y);
+
+    return 0;
+}
